b90caa9e2271d16d918f08293914bc0257a3e92e
Aleks Clapin-Pepin <aclapinpepin@gmail.com>
2021-06-28 12:31:17 +0200

    Initial commit

---
+++ Poutine.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract PTNToken is ERC20, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+
+    constructor(uint256 initialSupply) ERC20("Poutine", "PTN") {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+
+        mint(msg.sender, initialSupply);
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+      _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public onlyRole(BURNER_ROLE) {
+      _burn(from, amount);
+    }
+}
